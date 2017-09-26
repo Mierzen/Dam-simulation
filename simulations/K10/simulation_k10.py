3f268e84e266da4a90419dfd6ba819f7e63a9968
Stéphan Taljaard <steptaljaard@gmail.com>
2017-09-26 22:10:53 +0200

    Add K10 simulation

---
+++ simulations/K10/simulation_k10.py
@@ -0,0 +1,42 @@
+import modules.pumpingsystem as ps
+import pandas as pd
+import numpy as np
+
+# Pump schedule as per SCADA. rows = pumps, columns 1:=Peak, 2:=Standard, 3:Off-peak
+pump_schedule_27 = np.array([[80, 30, 30],
+                            [85, 40, 40],
+                            [90, 85, 80],
+                            [150, 150, 150],
+                            [150, 150, 150]])
+pump_schedule_12 = np.array([[80, 30, 30],
+                            [85, 40, 35],
+                            [90, 50, 40],
+                            [150, 150, 150],
+                            [150, 150, 150]])
+
+# Inflows into dams
+dam_inflow_profiles = pd.read_csv('K10_dam_inflow_profiles.csv.gz')
+inflow_27 = np.reshape(dam_inflow_profiles['27L Inflow'].values, (24, 2))
+inflow_12 = np.reshape(dam_inflow_profiles['12L Inflow'].values, (24, 2))
+
+# Read actual data for initial conditions and verification
+actual_values = pd.read_csv('K10_data_for_verification.csv.gz')
+actual_status_27 = actual_values['27L Status'].values
+actual_status_12 = actual_values['12L Status'].values
+initial_level_27 = actual_values['27L Level'][0]
+initial_level_12 = actual_values['12L Level'][0]
+
+# Create pump system
+pump_system = ps.PumpSystem('K10')
+pump_system.add_level(ps.PumpingLevel("27L", 3000000, initial_level_27,
+                                      236.1, 2925.6, pump_schedule_27, actual_status_27[0],
+                                      inflow_27, fed_to_level="12L", pump_statuses_for_verification=actual_status_27))
+pump_system.add_level(ps.PumpingLevel("12L", 3000000, initial_level_12,
+                                      194.6, 2656.6, pump_schedule_12, actual_status_12[0],
+                                      inflow_12, pump_statuses_for_verification=actual_status_12))
+
+
+# Perform simulations
+pump_system.perform_simulation(mode='verification', save=True)
+pump_system.perform_simulation(mode='1-factor', save=True)
+pump_system.perform_simulation(mode='2-factor', save=True)
